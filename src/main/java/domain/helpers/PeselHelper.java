7277713cea4643c65b821cd2ede88179f5df194f
student <student@virtualbox>
2016-12-10 13:37:28 +0100

    Dodane testy

--- src/main/java/domain/helpers/PeselHelper.java
+++ src/main/java/domain/helpers/PeselHelper.java
@@ -3,7 +3,28 @@ package domain.helpers;
 public class PeselHelper {
 
 	public static boolean check(String correctPesel) {
-		// TODO Auto-generated method stub
+		
+		try {
+			int pesel = Integer.parseInt(correctPesel);
+		} catch (NumberFormatException nfe) {
+			return false; 
+		}
+		
+		if( correctPesel.length() != 11) {
+			return false; 
+		}
+		
+		String pattern = "9731973197";
+		int checksum = 0;
+		for (int i=0; i<10; i++) {
+			checksum += Integer.parseInt(Character.toString(correctPesel.charAt(i)))
+						* Integer.parseInt(Character.toString(pattern.charAt(i)));
+		}
+		
+		if (Integer.toString(checksum%10) != Character.toString(correctPesel.charAt(11))) {
+			return false;
+		}
+		
 		return true;
 	}
 
